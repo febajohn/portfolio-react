454aef97a2348feded5df6eeea4618d3895f1059
febajohn <febamol22@gmail.com>
2025-08-28 14:32:36 +0530

    Initial portfolio commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-react/',  // 👈 Add this line
+  base: '/portfolio-react/',  // 👈 Must match your repo name
 })
